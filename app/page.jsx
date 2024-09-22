f7d45701b7071745092954b7b6f77718d2397183
Gigi Beridze <beridzegigi19@gmail.comecho>
2024-09-22 15:34:30 +0400

    Fix Console

--- app/page.jsx
+++ app/page.jsx
@@ -16,7 +16,10 @@ export default function Home() {
   
   const {isSignedIn, isLoaded, user} = useUser()
 
-
+  if (process.env.NODE_ENV === 'production') {
+    console.error = () => {}; // Suppress all console.error messages
+    console.warn = () => {}; // Optionally suppress warnings too
+}
   if(!isLoaded){
     return <ClerkLoading></ClerkLoading>
   }else{
